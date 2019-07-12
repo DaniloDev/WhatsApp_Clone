205895a85dc1a346ac2d4a81d8839caf3027c7ac
Danilo <danisn2010@gmail.com>
2019-07-12 13:34:17 -0300

    Scroll Automático
    Scroll Automático

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -187,12 +187,17 @@ export class WhatsAppController{
             display: 'flex'
         })
 
+        this.el.panelMessagesContainer.innerHTML = ''
+
         Message.getRef(this._contactActive.chatId)
         .orderBy('timeStamp')
         .onSnapshot(docs=>{
 
-            this.el.panelMessagesContainer.innerHTML = ''
-
+            let scrollTop = this.el.panelMessagesContainer.scrollTop
+            let scrollTopMax = (this.el.panelMessagesContainer.scrollHeight -
+            this.el.panelMessagesContainer.offsetHeight)
+            let autoScroll = (scrollTop >= scrollTopMax)
+                
             docs.forEach(doc=>{
 
                 let data = doc.data()
@@ -213,6 +218,16 @@ export class WhatsAppController{
                 }
 
             })
+
+            if(autoScroll){
+
+                this.el.panelMessagesContainer.scrollTop = 
+                (this.el.panelMessagesContainer.scrollHeight - 
+                    this.el.panelMessagesContainer.offsetHeight)
+            }else{
+
+                this.el.panelMessagesContainer.scrollTop = scrollTop
+            }
 
         })
 
