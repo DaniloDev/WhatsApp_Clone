58da0c8f7ebfa2a2ab76be950749086f9f5776fa
Danilo <danisn2010@gmail.com>
2019-07-13 00:13:57 -0300

    Atualizando Status das Mensagens
    Atualizando Status das Mensagens

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -211,18 +211,33 @@ export class WhatsAppController{
                 let data = doc.data()
 
                 data.id = doc.id
+
+                let message = new Message()
+
+                message.fromJSON(data)
+
+                let me = (data.from === this._user.email)
                
                 if(!this.el.panelMessagesContainer.querySelector('#_' + data.id)){
+           
+                    if(!me){
 
-                    let message = new Message()
-
-                    message.fromJSON(data)
-                    
-                    let me = (data.from === this._user.email)
+                        doc.ref.set({
+                            status: 'read'
+                        },{
+                            merge: true
+                        })
+                    }
 
                     let view = message.getViewElement(me)
 
                     this.el.panelMessagesContainer.appendChild(view)
+
+                }else if(me){
+
+                    let msgEL = this.el.panelMessagesContainer.querySelector('#_' + data.id)
+                      
+                    msgEL.querySelector('.message-status').innerHTML = message.getStatusViewElement().outerHTML
                 }
 
             })
